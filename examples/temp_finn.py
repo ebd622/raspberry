11510c9134650cdcff1c4a4efef6855df34b042c
Eugeny <eburdo@gmail.com>
2019-11-26 14:06:28 +0000

    add push notification

--- examples/temp_finn.py
+++ examples/temp_finn.py
@@ -1,4 +1,5 @@
 import time
+import requests
 from grove.factory import Factory
 
 # connect to alalog pin 2(slot A2)
@@ -11,12 +12,18 @@ print('Detecting temperature...')
 while True:
     t=sensor.temperature
 
-    if t>26 and not sent:
-      print('Too hot...')
+    if t<24 and not sent:
+      print('Too cold...')
+      data = '{"actionID":"D0B7EB88-9098-4FDD-BA88-1B973154D2AF"}'
+      url = "http://localhost:3001/actions"
+      headers = {'content-type': 'application/json'}
+      r = requests.post(url, data=data, headers=headers)
+
+      #curl -d '{"actionID":"D0B7EB88-9098-4FDD-BA88-1B973154D2AF"}' -H "Content-Type: application/json" http://localhost:3001/actions
       #TODO: push a notification
       sent=True
 
-    if t<26 and sent:
+    if t>24 and sent:
       #Reset the flag
       sent=False
  
